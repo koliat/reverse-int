797694b07a09845f5fe65562182af965095f6922
Николай Гучок <nikolajgucok@MacBook-Pro-Nikolaj.local>
2023-01-07 20:38:47 +0300

    solved task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let str = Math.abs(n).toString();
+    let result = str.split("").reverse().join("");
+    return result;
+};
